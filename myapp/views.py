a51c7a4fbef71e0054ebb054d605f55a8ec46353
taeyeon0319 <xodus1349@likelion.org>
2021-08-09 23:06:14 +0900

    Pagination

--- myapp/views.py
+++ myapp/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from .models import Person
 from .filters import PersonFilter
 
+from django.core.paginator import Paginator
+
 # Create your views here.
 
 def show_all_persons_page(request):
@@ -13,5 +15,11 @@ def show_all_persons_page(request):
     )
 
     context['filtered_persons'] = filtered_persons
+
+    paginated_filtered_persons = Paginator(filtered_persons.qs, 2)
+    page_number = request.GET.get('page')
+    person_page_obj = paginated_filtered_persons.get_page(page_number)
+
+    context['person_page_obj'] = person_page_obj
 
     return render(request, 'myapp/show_all_persons_page.html', context=context)
